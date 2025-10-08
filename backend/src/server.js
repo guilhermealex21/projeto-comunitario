d9560071ab956ef56ccfa1296a50c08d853713b0
guilhermealex21 <gabrielsilva6833@gmail.com>
2025-10-08 11:01:36 -0300

    Add server.js file for backend

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Rota de teste
+app.get("/", (req, res) => {
+  res.send("API da Plataforma de Transparência está rodando 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Servidor rodando na porta ${PORT}`)
+);
